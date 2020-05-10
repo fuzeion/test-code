24a901a8e961a1c8529d7069b6754daba43cf40d
John Brady <johntbrady@gmail.com>
2020-05-10 11:39:37 -0400

    testing SSH

--- src/Welcome.java
+++ src/Welcome.java
@@ -3,5 +3,6 @@ public class Welcome {
 	public static void main(String[] args) {
 		// Display message Welcome to Java! on the console
 		System.out.println("Welcome to Java!");
+		System.out.println("version 1");
 	}	
 }
