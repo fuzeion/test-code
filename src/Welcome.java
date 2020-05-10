e1a96c3fc881f510be5f591d6bb73d30c38ccc26
John Brady <johntbrady@gmail.com>
2020-05-10 11:15:21 -0400

    changed back to println

--- src/Welcome.java
+++ src/Welcome.java
@@ -2,6 +2,6 @@
 public class Welcome {
 	public static void main(String[] args) {
 		// Display message Welcome to Java! on the console
-		System.out.print("Welcome to Java!");
+		System.out.println("Welcome to Java!");
 	}	
 }
